11d5d1638d802411e3ab3d7d38102e3492207c5b
MarcMcArthur <mcarthur.marcrichard@gmail.com>
2016-04-06 13:30:37 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mcflixter::Application.config.secret_key_base = 'aa7d725a9d246c69bb68926612acc4812b13a0720e9e212b2d783e08884c5995c11cee39947cda84595c58d2af03c2372d98295f57678f9b11fc1d8dd995a716'
