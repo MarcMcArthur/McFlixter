0b99b9d6e27740360794411a6d8f4d8f9b1aa56a
MarcMcArthur <mcarthur.marcrichard@gmail.com>
2016-04-07 16:09:13 +0000

    Add devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Mcflixter::Application.configure do
+  config.action_mailer.default_url_options = { host: 'mcflixter-marc-mcarthur.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
